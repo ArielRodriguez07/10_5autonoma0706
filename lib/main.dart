8da913739525e2b7c4ebba8d3f5712f072be6c32
ARIEL_RODRIGUEZ_CENICEROS <a.22308051280706@cbtis128.edu.mx>
2025-03-04 16:42:34 +0000

    ariel

--- lib/main.dart
+++ lib/main.dart
@@ -1,41 +1,71 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  runApp(BorderApp());
+}
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-  // This widget is the root of your application.
+class BorderApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      // Application name
-      title: 'Flutter Hello World',
-      // Application theme data, you can set the colors for the application as
-      // you want
-      theme: ThemeData(
-        // useMaterial3: false,
-        primarySwatch: Colors.blue,
-      ),
-      // A widget which will be started on application startup
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatelessWidget {
-  final String title;
-  const MyHomePage({super.key, required this.title});  
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        // The title text which will be shown on the action bar
-        title: Text(title),
-      ),
-      body: Center(
-        child: Text(
-          'Hello, World!',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Center(
+            child: Text(
+              'Border Demo',
+              style: TextStyle(color: Colors.black),
+            ),
+          ),
+          backgroundColor: Color(0xff6ae9f9),
+          elevation: 0,
+        ),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.only(top: 20.0),
+              child: Center(
+                child: Image.network(
+                  'https://raw.githubusercontent.com/ArielRodriguez07/p10-5autonoma0706/refs/heads/main/dise%C3%B1o3.PNG',
+                  width: 200,
+                  height: 200,
+                  fit: BoxFit.contain,
+                ),
+              ),
+            ),
+            SizedBox(height: 10),
+            Text(
+              'Ariel Rodriguez Ceniceros 22308051280706',
+              style:
+                  TextStyle(fontSize: 20.0), // Aumenta el tamaño de la fuente
+            ),
+            SizedBox(height: 20),
+            Center(
+              child: Container(
+                width: 100,
+                height: 150,
+                decoration: BoxDecoration(
+                  border: Border(
+                    top: BorderSide(color: Color(0xff5fd9ef), width: 2),
+                    bottom: BorderSide(color: Color(0xff4de8ed), width: 2),
+                    left: BorderSide(color: Colors.black, width: 2),
+                    right: BorderSide(color: Colors.black, width: 2),
+                  ),
+                  borderRadius: BorderRadius.circular(8),
+                ),
+                child: Align(
+                  alignment: Alignment.topCenter,
+                  child: Padding(
+                    padding: const EdgeInsets.only(top: 10.0),
+                    child: Text(
+                      'D',
+                      style: TextStyle(fontSize: 48),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ],
         ),
       ),
     );
